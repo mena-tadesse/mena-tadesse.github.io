1eecd6e6b7f7dcc8f7577554a528a0285e27093a
Mena Tadesse <137219749+mena-tadesse@users.noreply.github.com>
2023-10-01 18:41:43 -0400

    dates

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -4,3 +4,13 @@ function scriptTest(){
 }
 
 var onclick = scriptTest();
+
+function dateTime(){
+    var today = new Date();
+    print("Today's date is: " + document.toDateString());
+
+}
+
+//Today is 3:53pm on Wednesday, 3 November, 2027
+
+var onToday = dateTime();
