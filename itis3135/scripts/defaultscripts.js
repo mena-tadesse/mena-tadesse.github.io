44ac41f2b92006b0e5e3a0b9f8879807d5ba8cb0
Mena Tadesse <137219749+mena-tadesse@users.noreply.github.com>
2023-10-01 23:14:54 -0400

    changes

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,4 +1,9 @@
-
+function welcome(){
+    var fname = document.getElementById('firstName');
+    var lname = document.getElementById('lastName');
+    var mood = document.getElementById('moody');
+    alert("Tadesse Designs welcomes you " + fname.value + " " + lname.value + " ! We are glad you are doing " + mood + "!");
+}
 
 
 
