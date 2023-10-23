17a283eb6ab70627ab5ccb678c0fab88d132e316
Mena Tadesse <137219749+mena-tadesse@users.noreply.github.com>
2023-10-23 00:01:38 -0400

    practice calculator

---
+++ itis3135/scripts/calculator.js
@@ -0,0 +1,27 @@
+function add(){
+    var num1 = document.getElementByClass("buttons").value;
+    var num2 = document.getElementByClass("buttons").value;
+    var total = num1 + num2;
+    alert(total);
+}
+
+function subtract(){
+    var num1 = document.getElementByClass("buttons").value;
+    var num2 = document.getElementByClass("buttons").value;
+    var total = num1 - num2;
+    alert(total);
+}
+
+function multiply(){
+    var num1 = document.getElementByClass("buttons").value;
+    var num2 = document.getElementByClass("buttons").value;
+    var total = num1 * num2;
+    alert(total);
+}
+
+function divide(){
+    var num1 = document.getElementByClass("buttons").value;
+    var num2 = document.getElementByClass("buttons").value;
+    var total = num1 / num2;
+    alert(total);
+}
