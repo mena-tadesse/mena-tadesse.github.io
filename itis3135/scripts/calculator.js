d337c67ec8c5039f2d21321e3cee31f4a9d69dc1
Mena Tadesse <137219749+mena-tadesse@users.noreply.github.com>
2023-12-04 23:13:49 -0500

    calc

--- itis3135/scripts/calculator.js
+++ itis3135/scripts/calculator.js
@@ -1,27 +1,70 @@
-function add(){
-    var num1 = document.getElementByClass("buttons").value;
-    var num2 = document.getElementByClass("buttons").value;
-    var total = num1 + num2;
-    alert(total);
+let zero = 0;
+let one = 1;
+let two = 2; 
+let three = 3;
+let four = 4;
+let five = 5;
+let six = 6;
+let seven = 7;
+let eight = 8;
+let nine = 9;
+let add = '+';
+let subtract = '-';
+let multiply = '*';
+let divide = '/';
+
+function show(insert) {
+    if(insert == zero){
+    document.getElementById("output").value += zero;  
+    }
+    else if(insert == one){
+        document.getElementById("output").value += one;
+    }
+    else if(insert == two){
+        document.getElementById("output").value += two;
+    }
+    else if(insert == three){
+        document.getElementById("output").value += three;
+    }
+    else if(insert == four){
+        document.getElementById("output").value += four;
+    }
+    else if(insert == five){
+        document.getElementById("output").value += five;
+    }
+    else if(insert == six){
+        document.getElementById("output").value += six;
+    }
+    else if(insert == seven){
+        document.getElementById("output").value += seven;
+    }
+    else if(insert == eight){
+        document.getElementById("output").value += eight;
+    }
+    else if(insert == nine){
+        document.getElementById("output").value += nine;
+    }
+    else if(insert == "+"){
+        document.getElementById("output").value += insert;
+    }
+    else if(insert == "-"){
+        document.getElementById("output").value += insert;
+    }
+    else if(insert == "*"){
+        document.getElementById("output").value += insert;
+    }
+    else if(insert == "/"){
+        document.getElementById("output").value += insert;
+    }
+    else if(insert == "."){
+        document.getElementById("output").value += insert;
+    }
 }
 
-function subtract(){
-    var num1 = document.getElementByClass("buttons").value;
-    var num2 = document.getElementByClass("buttons").value;
-    var total = num1 - num2;
-    alert(total);
+function calculate() {
+    document.getElementById("output").value = eval(output.value);
 }
 
-function multiply(){
-    var num1 = document.getElementByClass("buttons").value;
-    var num2 = document.getElementByClass("buttons").value;
-    var total = num1 * num2;
-    alert(total);
-}
-
-function divide(){
-    var num1 = document.getElementByClass("buttons").value;
-    var num2 = document.getElementByClass("buttons").value;
-    var total = num1 / num2;
-    alert(total);
+function cleared() {
+    document.getElementById("output").value = "";
 }
