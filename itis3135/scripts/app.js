1107f0af6cdf0cf67372248d83a5946557aa4002
Mena Tadesse <137219749+mena-tadesse@users.noreply.github.com>
2023-12-09 12:57:17 -0500

    pop up!!! im so happy this works!!! XD

--- itis3135/scripts/app.js
+++ itis3135/scripts/app.js
@@ -1,5 +1,9 @@
-
 function popUp(){
     document.getElementById("popup").style.display = "block";
     document.getElementById("appointmentSection").style.display = "none";
+}
+
+function closePopUp(){
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("appointmentSection").style.display = "block";
 }
