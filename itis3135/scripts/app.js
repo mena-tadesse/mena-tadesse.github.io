d1ddadd97f4a751aeb7e3ad6c40cb516a07db05a
Mena Tadesse <137219749+mena-tadesse@users.noreply.github.com>
2023-12-10 14:27:07 -0500

    functionalities and new image

--- itis3135/scripts/app.js
+++ itis3135/scripts/app.js
@@ -1,3 +1,4 @@
+/*Appointment*/
 function popUp(){
     document.getElementById("popup").style.display = "block";
     document.getElementById("appointmentSection").style.display = "none";
@@ -11,3 +12,63 @@ function closePopUp(){
 function clearForm() {
     document.getElementById('appointmentForm').reset();
 }
+
+/*Review*/
+function addReview(){
+    if(document.getElementById("userReview").value != "" && document.getElementById("name").value != ""){
+    document.getElementById("reviewsWillGoHere").innerHTML += "<h2>";
+    document.getElementById("reviewsWillGoHere").innerHTML += "<p>" + document.getElementById("userReview").value + " ~ " + document.getElementById("name").value + "</p>";
+    document.getElementById("reviewsWillGoHere").innerHTML += "</h2>";
+    }
+}
+
+function clearReview(){
+    document.getElementById('name').value = '';
+    document.getElementById('userReview').value = '';
+}
+
+/*Our Team*/
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+
+/* 
+let slideIndex = 1;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slides");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+*/
